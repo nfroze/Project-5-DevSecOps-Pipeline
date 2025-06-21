aaf6c9974fb930831c08fe2b2e0a9f72c124a3d9
Noah <noahfrost@outlook.com>
2025-06-21 20:47:01 +0100

    Update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
+  default     = "10.0.0.0/16"  # Keep same unless you want to change
 }
 
 variable "azs" {
