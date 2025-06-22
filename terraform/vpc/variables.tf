318b433e0901231aa7fddad5f30162f82b2c1d7a
Noah <noahfrost@outlook.com>
2025-06-22 10:40:46 +0100

    Update

--- terraform/vpc/variables.tf
+++ terraform/vpc/variables.tf
@@ -7,3 +7,15 @@ variable "azs" {
   description = "Availability Zones for subnets"
   type        = list(string)
 }
+
+variable "public_subnet_count" {
+  description = "Number of public subnets to create"
+  type        = number
+  default     = 2
+}
+
+variable "private_subnet_count" {
+  description = "Number of private subnets to create"
+  type        = number
+  default     = 2
+}
