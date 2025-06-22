c82cc0ad2456699d26f05eb4a01f5989b824d77c
Noah <noahfrost@outlook.com>
2025-06-22 01:40:31 +0100

    Update

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,3 +1,22 @@
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+  }
+
+  enabled_cluster_log_types = ["api", "audit", "authenticator"]
+
+  tags = {
+    Name = var.cluster_name
+  }
+
+  depends_on = [
+    var.cluster_role_arn
+  ]
+}
+
 resource "aws_eks_node_group" "default" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-nodes"
@@ -11,10 +30,6 @@ resource "aws_eks_node_group" "default" {
   }
 
   instance_types = ["t3.medium"]
-
-  metadata_options {
-    instance_metadata_tags = "disabled"
-  }
 
   tags = {
     Name = "${var.cluster_name}-node-group"
