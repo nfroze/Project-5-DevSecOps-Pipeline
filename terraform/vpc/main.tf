06ab9e37cb5eb1dd91eeb1e29c9ee5dacef6608e
Noah <noahfrost@outlook.com>
2025-06-22 01:53:34 +0100

    Update

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -16,6 +16,22 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_eip" "nat" {
+  vpc = true
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "project5-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
 resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
@@ -24,7 +40,59 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "project5-public-subnet-${count.index}"
+    Name                                      = "project5-public-subnet-${count.index}"
     "kubernetes.io/cluster/project5-eks-cluster" = "owned"
+    "kubernetes.io/role/elb"                 = "1"
   }
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet(var.vpc_cidr, 8, count.index + 2)
+  availability_zone = element(var.azs, count.index)
+
+  tags = {
+    Name                                      = "project5-private-subnet-${count.index}"
+    "kubernetes.io/cluster/project5-eks-cluster" = "owned"
+    "kubernetes.io/role/internal-elb"        = "1"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "project5-public-rt"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "project5-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
 }
