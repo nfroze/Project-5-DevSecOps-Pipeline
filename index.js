6484c3d3fe80fb37d53094553d91273038ee273f
Noah <noahfrost@outlook.com>
2025-06-24 16:30:16 +0100

    Update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const userRoutes = require('./routes/user');
 
 const app = express();
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 
 app.use('/user', userRoutes);
