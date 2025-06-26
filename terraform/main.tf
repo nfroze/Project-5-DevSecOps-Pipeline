9488e4e1ce594aa0532134e909ac4c7ccf4493f0
Noah <noahfrost@outlook.com>
2025-06-26 23:49:37 +0100

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  required_version = ">= 1.3.0"
-}
-
 provider "aws" {
   region = var.aws_region
 }
 
 module "vpc" {
-  source   = "./vpc"
+  source = "./vpc"
+
   vpc_cidr = var.vpc_cidr
   azs      = var.azs
 }
@@ -24,10 +14,20 @@ module "iam" {
 }
 
 module "eks" {
-  source              = "./eks"
-  vpc_id              = module.vpc.vpc_id
-  subnet_ids          = module.vpc.public_subnet_ids
-  cluster_name        = var.cluster_name
-  cluster_role_arn    = module.iam.eks_cluster_role_arn
-  node_group_role_arn = module.iam.eks_node_role_arn
+  source = "./eks"
+
+  cluster_name              = var.cluster_name
+  cluster_role_arn          = module.iam.eks_cluster_role_arn
+  subnet_ids                = module.vpc.private_subnet_ids
+
+  node_group_name           = var.node_group_name
+  node_role_arn             = var.node_role_arn
+  desired_capacity          = var.desired_capacity
+  min_capacity              = var.min_capacity
+  max_capacity              = var.max_capacity
+  instance_types            = var.instance_types
+  ami_type                  = var.ami_type
+  ssh_key                   = var.ssh_key
+  source_security_group_ids = var.source_security_group_ids
+  kms_key_arn               = var.kms_key_arn
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "eu-west-2"
 }
@@ -7,17 +7,71 @@ variable "aws_region" {
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"  # Keep same unless you want to change
+  default     = "10.0.0.0/16"
 }
 
 variable "azs" {
-  description = "Availability Zones"
+  description = "List of availability zones"
   type        = list(string)
   default     = ["eu-west-2a", "eu-west-2b"]
 }
 
 variable "cluster_name" {
-  description = "EKS Cluster Name"
+  description = "Name of the EKS cluster"
   type        = string
-  default     = "project5-eks-cluster"
+}
+
+variable "node_group_name" {
+  description = "Name of the EKS node group"
+  type        = string
+}
+
+variable "node_role_arn" {
+  description = "IAM role ARN for the node group"
+  type        = string
+}
+
+variable "desired_capacity" {
+  description = "Desired number of nodes in the node group"
+  type        = number
+  default     = 2
+}
+
+variable "min_capacity" {
+  description = "Minimum number of nodes in the node group"
+  type        = number
+  default     = 1
+}
+
+variable "max_capacity" {
+  description = "Maximum number of nodes in the node group"
+  type        = number
+  default     = 3
+}
+
+variable "instance_types" {
+  description = "List of EC2 instance types for the node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "ami_type" {
+  description = "Type of AMI to use for the node group"
+  type        = string
+  default     = "AL2_x86_64"
+}
+
+variable "ssh_key" {
+  description = "Name of the EC2 Key Pair to enable SSH access"
+  type        = string
+}
+
+variable "source_security_group_ids" {
+  description = "List of source security group IDs for SSH access"
+  type        = list(string)
+}
+
+variable "kms_key_arn" {
+  description = "KMS key ARN for encrypting secrets in EKS"
+  type        = string
 }
