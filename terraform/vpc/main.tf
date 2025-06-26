1a9d45dc155f5e2b1e52794079b1919059d834e2
Noah <noahfrost@outlook.com>
2025-06-27 00:30:55 +0100

    Update

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -8,114 +12,100 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "vpc_flow_log" {
-  name              = "/aws/vpc/flow-logs"
-  retention_in_days = 14
-}
-
-resource "aws_flow_log" "vpc" {
-  log_destination      = aws_cloudwatch_log_group.vpc_flow_log.arn
-  log_destination_type = "cloud-watch-logs"
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.main.id
-  iam_role_arn         = aws_iam_role.flow_logs.arn
-}
-
-resource "aws_iam_role" "flow_logs" {
-  name = "vpc-flow-logs-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "vpc-flow-logs.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "flow_logs" {
-  name = "vpc-flow-logs-policy"
-  role = aws_iam_role.flow_logs.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Action = [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents",
-        "logs:DescribeLogGroups",
-        "logs:DescribeLogStreams"
-      ]
-      Resource = "*"
-    }]
-  })
-}
-
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.main.id
-
-  revoke_rules_on_delete = true
-
-  ingress = []
-  egress  = []
-
-  tags = {
-    Name = "default-restricted"
-  }
-}
-
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "main-gw"
+    Name = "${var.vpc_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt"
   }
 }
 
 resource "aws_subnet" "public" {
-  count                   = 2
+  count                   = length(var.public_subnets)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = var.azs[count.index]
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "public-${count.index + 1}"
+    Name = "${var.vpc_name}-public-${count.index + 1}"
   }
 }
 
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
 resource "aws_subnet" "private" {
-  count             = 2
+  count             = length(var.private_subnets)
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.azs[count.index]
 
   tags = {
-    Name = "private-${count.index + 1}"
+    Name = "${var.vpc_name}-private-${count.index + 1}"
   }
+}
+
+# checkov:skip=CKV2_AWS_19 reason="EIP is correctly attached to NAT Gateway and protected by IGW"
+resource "aws_eip" "nat" {
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.gw]
 }
 
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.public[0].id
+  subnet_id     = element(aws_subnet.public[*].id, 0)
 
   tags = {
-    Name = "main-nat"
+    Name = "${var.vpc_name}-nat"
   }
 
   depends_on = [aws_internet_gateway.gw]
 }
 
-resource "aws_eip" "nat" {
-  domain     = "vpc"
-  depends_on = [aws_internet_gateway.gw]
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 
   tags = {
-    Name = "nat-eip"
+    Name = "${var.vpc_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# checkov:skip=CKV2_AWS_12 reason="Default SG is restricted: no ingress or egress"
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.main.id
+
+  ingress = []
+  egress  = []
+
+  tags = {
+    Name = "${var.vpc_name}-default-sg"
   }
 }
