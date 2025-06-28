8433188594d1fc352ec32a185e974129fdeb19da
Noah <noahfrost@outlook.com>
2025-06-28 14:47:43 +0100

    Update

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
+const cookieParser = require('cookie-parser'); // required for csurf
+const csrf = require('csurf'); // CSRF middleware
 const userRoutes = require('./routes/user');
 
 const app = express();
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+const csrfProtection = csrf({ cookie: true });
+app.use(csrfProtection);
+
 const PORT = process.env.PORT || 3000;
+
+app.get('/csrf-token', (req, res) => {
+  res.json({ csrfToken: req.csrfToken() });
+});
 
 app.use('/user', userRoutes);
 
