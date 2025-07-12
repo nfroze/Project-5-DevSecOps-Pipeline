7778c11da8f58d41df76923b58ec46a0d2f5f8f4
Noah <noahfrost@outlook.com>
2025-07-12 19:46:01 +0100

    Update

---
+++ AWS-Infrastructure/modules/security/lambda/cloudwatch_handler.py
@@ -0,0 +1,39 @@
+﻿import base64
+import gzip
+import json
+import os
+import urllib3
+import uuid
+
+def handler(event, context):
+    splunk_url = os.environ['SPLUNK_HEC_URL']
+    splunk_token = os.environ['SPLUNK_HEC_TOKEN']
+    
+    http = urllib3.PoolManager()
+    
+    # Decode CloudWatch Logs data
+    log_data = json.loads(gzip.decompress(base64.b64decode(event['awslogs']['data'])))
+    
+    for log_event in log_data['logEvents']:
+        splunk_event = {
+            "event": {
+                "message": log_event['message'],
+                "logGroup": log_data['logGroup'],
+                "logStream": log_data['logStream'],
+                "timestamp": log_event['timestamp']
+            },
+            "sourcetype": "aws:cloudwatch",
+            "channel": str(uuid.uuid4())
+        }
+        
+        http.request(
+            'POST',
+            splunk_url,
+            body=json.dumps(splunk_event),
+            headers={
+                'Authorization': f'Splunk {splunk_token}',
+                'Content-Type': 'application/json'
+            }
+        )
+    
+    return {'statusCode': 200}
---
+++ AWS-Infrastructure/modules/security/lambda/guardduty_handler.py
@@ -0,0 +1,31 @@
+﻿import json
+import os
+import urllib3
+import uuid
+
+def handler(event, context):
+    splunk_url = os.environ['SPLUNK_HEC_URL']
+    splunk_token = os.environ['SPLUNK_HEC_TOKEN']
+    
+    http = urllib3.PoolManager()
+    
+    splunk_event = {
+        "event": event['detail'],
+        "sourcetype": "aws:guardduty",
+        "channel": str(uuid.uuid4())
+    }
+    
+    response = http.request(
+        'POST',
+        splunk_url,
+        body=json.dumps(splunk_event),
+        headers={
+            'Authorization': f'Splunk {splunk_token}',
+            'Content-Type': 'application/json'
+        }
+    )
+    
+    return {
+        'statusCode': response.status,
+        'body': response.data.decode('utf-8')
+    }
